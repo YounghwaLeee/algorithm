dd9d2871bc7a8b02fed802765127dba6ee9d1514
YounghwaLeee <89582828+YounghwaLeee@users.noreply.github.com>
2021-11-25 14:48:57 +0900

    java
    평균값 구하기

---
+++ Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+    public static double solution(int[] arr) {
+       
+    	double answer = 0;
+    	 int sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum+=arr[i];           
+        }
+        answer=(double)sum/arr.length;
+        
+        return answer;
+    
+}
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4};
+		solution(arr);
+		System.out.println(solution(arr));
+
+		
+	}
+}
