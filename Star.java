dbd11b65909e79f5c774d84d7b82a3d249d3352a
YounghwaLeee <89582828+YounghwaLeee@users.noreply.github.com>
2021-11-30 23:28:50 +0900

    star
    *****
    ****
    ***
    **
    *

---
+++ Star.java
@@ -0,0 +1,21 @@
+
+public class Star {
+	
+
+	public  Star() {
+		
+		
+		
+		for(int i=0; i<=5; i++) {
+			for(int j=0; i+j<5; j++) {
+				System.out.print("*");
+			}
+				System.out.println("");
+		}
+		
+	}
+	 public static void  main(String[] args){
+	 Star star = new Star();
+
+	 }
+}
